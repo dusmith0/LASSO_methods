8ee90bbb2dc10028c4bf43dd5fb62231ded52b2a
Dustin Smith <dusmith0@tamu.edu>
2023-11-06 22:15:54 -0600

    I added tests directly from example 2. My lasso objective seems to be producing many values instead of a single loss.

--- Test_LassoFunctions.R
+++ Test_LassoFunctions.R
@@ -25,3 +25,34 @@ sqrt(t(new$Xtilde) %*% new$Xtilde / n)
 new$Xtilde
 
 lasso(new$Xtilde,new$Ytilde,beta,.4)
+
+##Using the exmaple data to see if I am receiving the same output so far.
+n = 50
+beta = c(1, 0.5)
+beta0 = 2 
+p = length(beta)
+sigma = 0.4 
+
+library(mnormt)
+set.seed(983645) 
+
+Sigma = matrix(0.7, p, p) + diag(rep(1-0.7, p)) 
+X = rmnorm(n, mean = rep(0, p), varcov = Sigma) 
+
+Y = beta0 + X %*% beta + sigma * rnorm(n)
+
+new <- standardizeXY(X,Y)
+n <- nrow(X)
+sqrt(t(new$Xtilde) %*% new$Xtilde / n)
+
+##Example 2 centered calculations
+meanY = mean(Y)
+Ytilde = Y - mean(Y)
+meansX = colMeans(X)
+
+Xcentered = X - matrix(meansX, nrow(X), ncol(X), byrow = T)
+
+normsX = colSums(Xcentered^2)/n
+Xtilde = Xcentered %*% diag(1/sqrt(normsX))
+
+lasso(Xtilde,Ytilde,beta,.4)
