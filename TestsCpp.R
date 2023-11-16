5ca91e3c1f341b7495e7f6291cb27c887c6efd0f
Dustin Smith <dusmith0@tamu.edu>
2023-11-15 22:56:14 -0600

    I added some expect_equal parts to my script in TestCpp.R

--- TestsCpp.R
+++ TestsCpp.R
@@ -2,6 +2,7 @@
 # Header for Rcpp and RcppArmadillo
 library(Rcpp)
 library(RcppArmadillo)
+library(testthat)
 
 # Source your C++ funcitons
 sourceCpp("LassoInC.cpp")
@@ -11,34 +12,53 @@ source("LassoFunctions.R")
 
 # Do at least 2 tests for soft-thresholding function below. You are checking output agreements on at least 2 separate inputs
 #################################################
-# Test one: Super simple test to see if my function even works
-soft_c(3,2)
-soft_c(1,2)
-soft_c(-3,2)
+  # Test one: Super simple test to see if my function even works
+  expect_equal(soft_c(3,2),1)
+  expect_equal(soft_c(1,2),0)
+  expect_equal(soft_c(-3,2),-1)
 
+  # Test two using the LASSO objective
+  n <- length(Ytilde)
+  a <- sum((Ytilde - Xtilde %*% beta) ^ 2)/(2 * n) + lambda * sum(abs(beta))
+  expect_equal(soft_c(a,.02),soft(a,.02))
+  expect_equal(soft_c(a,.2),soft(a,.2))
+  expect_equal(soft_c(a,.1),soft(a,.1))
 
 # Do at least 2 tests for lasso objective function below. You are checking output agreements on at least 2 separate inputs
 #################################################
-# Test one using LASSO Example 2 Data.
-n = 50
-beta = c(1, 0.5)
-beta0 = 2 
-p = length(beta)
-sigma = 0.4 
+  # Test one using LASSO Example 2 Data from the class notes.
+  n = 50
+  beta = c(1, 0.5)
+  beta0 = 2 
+  p = length(beta)
+  sigma = 0.4 
 
-library(mnormt)
-set.seed(983645) 
+  library(mnormt)
+  set.seed(983645) 
 
-Sigma = matrix(0.7, p, p) + diag(rep(1-0.7, p)) 
-X = rmnorm(n, mean = rep(0, p), varcov = Sigma) 
+  Sigma = matrix(0.7, p, p) + diag(rep(1-0.7, p)) 
+  X = rmnorm(n, mean = rep(0, p), varcov = Sigma) 
 
-Y = beta0 + X %*% beta + sigma * rnorm(n)
-lambda <- .4 
-new <- standardizeXY(X,Y)
+  Y = beta0 + X %*% beta + sigma * rnorm(n)
+  lambda <- .4 
+  new <- standardizeXY(X,Y)
 
-lasso_c(new$Xtilde,new$Ytilde,beta,lambda)
-lasso(new$Xtilde,new$Ytilde,beta,lambda)
+  expect_equal(lasso_c(new$Xtilde,new$Ytilde,beta,lambda),lasso(new$Xtilde,new$Ytilde,beta,lambda))
+     # No value was found, the functons are equal.
 
+  # Test 2 using the RiboflavinData:
+  library(hdi)
+  data(riboflavin) 
+  dim(riboflavin$x)
+  class(riboflavin$x) <- class(riboflavin$x)[-match("AsIs", class(riboflavin$x))]
+  X = as.matrix(riboflavin$x)
+  Y = riboflavin$y
+  
+  new <- standardizeXY(X,Y)
+  lambda <- .04
+  expect_equal(lasso_c(new$Xtilde,new$Ytilde,beta,lambda),lasso(new$Xtilde,new$Ytilde,beta,lambda))
+    #Again no value was found so the functions are equal.
+  
 # Do at least 2 tests for fitLASSOstandardized function below. You are checking output agreements on at least 2 separate inputs
 #################################################
 
